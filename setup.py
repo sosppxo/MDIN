301875a0720464612530d08a59a4cce3395f840f
Liuuuuuuyh <138308044+Liuuuuuyh@users.noreply.github.com>
2024-08-19 15:40:17 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+        name='mdin',
+        version='1.0',
+        description='3D-GRES: Generalized 3D Referring Expression Segmentation',
+        author='Changli Wu',
+        author_email='wuchangli@stu.xmu.edu.cn',
+        packages=['mdin'],
+    )
