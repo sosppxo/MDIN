4cd474459d13ff75f3a4ff2859062f8f2be44ebd
Liuuuuuuyh <138308044+Liuuuuuyh@users.noreply.github.com>
2024-08-20 11:45:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 if __name__ == '__main__':
     setup(
-        name='mdin',
+        name='gres_model',
         version='1.0',
         description='3D-GRES: Generalized 3D Referring Expression Segmentation',
         author='Changli Wu',
         author_email='wuchangli@stu.xmu.edu.cn',
-        packages=['mdin'],
+        packages=['gres_model'],
     )
